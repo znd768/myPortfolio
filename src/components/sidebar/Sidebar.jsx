99db99691155ee58a9d6069812a05996e7ac32c3
znd666 <rasksk37@gmail.com>
2024-08-03 23:47:08 +0900

    added darkmode toggle button for mobile

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,6 +3,7 @@ import ToggleButton from "./toggleButton/ToggleButton";
 import Navigation from "./toggleButton/Navigation";
 import { AnimatePresence, motion, useCycle } from "framer-motion";
 import { useLocation } from "react-router-dom";
+import { useTheme, useThemeUpdate } from "../ThemeContext";
 
 const variants = {
   open: {
@@ -27,6 +28,8 @@ const Sidebar = () => {
   const [isOpen, toggleOpen] = useCycle(false, true);
   const containerRef = useRef(null);
   const location = useLocation();
+  const darkTheme = useTheme();
+  const themeUpdate = useThemeUpdate();
 
   useEffect(() => {
     if (isOpen) {
@@ -45,9 +48,36 @@ const Sidebar = () => {
         variants={variants}
         className="absolute right-0 top-0 z-20 h-full w-[300px] bg-light-theme-1 dark:bg-dark-theme-1"
       >
-        <div className=" h-screen w-full grid place-items-center">
+        <div className="grid h-screen w-full place-items-center">
           <Navigation />
         </div>
+        <button
+          className="absolute left-[12px] top-[12px] grid size-[56px] place-items-center rounded-full border-2 border-transparent hover:border-light-theme-text hover:dark:border-white duration-200"
+          onClick={themeUpdate}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.8}
+            stroke="currentColor"
+            className="size-6"
+          >
+            {darkTheme ? (
+              <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+            />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+              />
+            )}
+          </svg>
+        </button>
       </motion.div>
       <ToggleButton toggle={toggleOpen} />
       <AnimatePresence>
