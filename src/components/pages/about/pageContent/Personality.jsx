6281268ce10240838ccdc646ded68d635e156911
znd768 <rasksk37@gmail.com>
2024-08-28 21:18:40 +0900

    added auto horizontal scroll

--- src/components/pages/about/pageContent/Personality.jsx
+++ src/components/pages/about/pageContent/Personality.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { useScroll, motion, useMotionValueEvent } from "framer-motion";
+import React, { useRef } from "react";
+import { useDimensions } from "../../../pageFunctions/useDimensions";
 
 const personalInfo = [
   {
@@ -23,45 +25,89 @@ const personalInfo = [
 ];
 
 const Personality = () => {
+  const containerRef = useRef(null);
+  const autoScrollRef = useRef(null);
+  const dimensionRef = useRef(null);
+
+  const dimensions = useDimensions(dimensionRef);
+  const contentHeight = (dimensions.width * personalInfo.length) / 2;
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+  });
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    const scrollElem = autoScrollRef.current;
+
+    scrollElem.scrollTo({
+      top: 0,
+      left: latest * dimensions.width * personalInfo.length,
+      behavior: "smooth",
+    });
+  });
+
+  const handleKeyDown = (e) => {
+    if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+      e.preventDefault();
+    }
+  };
+
   return (
-    <section className="flex min-h-[calc(100vh-5rem)] flex-col gap-8 px-6 py-4 lg:p-8 xl:p-10">
-      <h1 className="text-4xl">About Me</h1>
-      <h2 className="text-xl lg:text-2xl">My Personality</h2>
-      <article className="flex flex-col">
-        {personalInfo.map((info) => {
-          return (
-            <div className="flex h-[600px] w-full flex-row" key={info.title}>
-              <div className="flex w-[40%] flex-col justify-center gap-4 p-12">
-                <h3 className="text-lg lg:text-2xl">{info.title}</h3>
-                <ul className="ml-8">
-                  {Object.entries(info.items).map(([itemKey, item]) => {
-                    return (
-                      <li key={itemKey} className="first-letter:uppercase">
-                        {info.putItemName && (
-                          <span className="mr-2 after:content-[':']">
-                            {itemKey}
-                          </span>
-                        )}
-                        {item}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
-              <div className="w-[60%]"></div>
-            </div>
-          );
-        })}
+    <motion.section
+      className="relative"
+      ref={containerRef}
+      style={{ height: `${contentHeight}px` }}
+    >
+      <div className="sticky top-0 h-screen px-6 py-4 lg:p-8 xl:p-10">
         <div>
+          <h1 className="text-4xl">About Me</h1>
+          <h2 className="text-xl lg:text-2xl">My Personality</h2>
+        </div>
+        <motion.article
+          className="flex w-[100vw] max-w-[100%] snap-x snap-proximity flex-row flex-nowrap overflow-x-scroll no-scrollbar"
+          ref={autoScrollRef}
+          onKeyDown={handleKeyDown}
+          tabIndex={0}
+        >
+          {personalInfo.map((info) => {
+            return (
+              <div
+                className="flex h-[600px] w-full shrink-0 snap-end flex-row"
+                key={info.title}
+                ref={dimensionRef}
+              >
+                <div className="flex w-[40%] flex-col justify-center gap-4 p-12">
+                  <h3 className="text-lg lg:text-2xl">{info.title}</h3>
+                  <ul className="ml-8">
+                    {Object.entries(info.items).map(([itemKey, item]) => {
+                      return (
+                        <li key={itemKey} className="first-letter:uppercase">
+                          {info.putItemName && (
+                            <span className="mr-2 after:content-[':']">
+                              {itemKey}
+                            </span>
+                          )}
+                          {item}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+                <div className="w-[60%]"></div>
+              </div>
+            );
+          })}
+          {/* <div>
           <h3 className="text-2xl">How to spend my holidays</h3>
           <p>I spend almost time staying home.</p>
           <p>
             I hardly go out except purpose of a hospital visit or shopping
             because I dislike crowd... :)
           </p>
-        </div>
-      </article>
-    </section>
+        </div> */}
+        </motion.article>
+      </div>
+    </motion.section>
   );
 };
 
