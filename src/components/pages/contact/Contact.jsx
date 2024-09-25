c2eef4380ab55d9709b83f9b685384c1aef5e6e5
znd768 <rasksk37@gmail.com>
2024-09-25 23:38:38 +0900

    z-index bugfix

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -86,7 +86,7 @@ const Contact = () => {
   };
 
   return (
-    <section className="relative mx-auto grid min-h-[calc(100vh-5rem)] max-w-[1344px] auto-rows-min grid-cols-20 gap-2 md:gap-4 lg:gap-y-0">
+    <section className="relative mx-auto grid min-h-[calc(100vh-5rem)] max-w-[1344px] auto-rows-min grid-cols-20 gap-2 md:gap-4 lg:gap-y-0 z-0">
       <h1 className="col-start-1 col-end-8 flex h-12 items-center text-2xl md:col-start-2 md:col-end-18 lg:h-24 lg:text-3xl xl:text-4xl">
         Contact
       </h1>
