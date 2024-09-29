c56547fb8485a483e95a3139df0b44d7fb0ce776
znd768 <rasksk37@gmail.com>
2024-09-29 17:18:16 +0900

    try to fix env variables config

--- src/components/pages/homepage/Home.jsx
+++ src/components/pages/homepage/Home.jsx
@@ -3,8 +3,6 @@ import Description from "./content/Description";
 import ProfImage from "./content/ProfImage";
 
 const Home = () => {
-  console.log(import.meta.env.VITE_SITE_RECAPTCHA_KEY);
-
   return (
     <div className="mt-12 grid min-h-[calc(100vh-8rem)] place-items-center p-4 sm:mt-0 sm:min-h-[calc(100vh-5rem)] lg:p-12">
       <div className="flex max-w-[1344px] flex-col-reverse items-center justify-evenly gap-8 lg:flex-row lg:gap-0 xl:gap-12">
