a119645a0d6e71f9d6f8f0da3543022f967167af
znd768 <rasksk37@gmail.com>
2024-09-29 16:47:40 +0900

    added google recaptcha

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -1,90 +1,7 @@
-import React, { useState } from "react";
-import FormInput from "./FormInput";
-
-const inputList = [
-  {
-    tag: "textarea",
-    id: "message",
-    name: "message",
-    type: "text",
-    label: "Message",
-    required: true,
-    rows: 8,
-  },
-  {
-    tag: "input",
-    id: "name",
-    name: "name",
-    type: "text",
-    label: "Your Name",
-    placeholder: "",
-    required: true,
-  },
-  {
-    tag: "input",
-    id: "company-name",
-    name: "companyName",
-    type: "text",
-    label: "Company Name",
-    placeholder: "Example Company",
-    required: false,
-  },
-  {
-    tag: "input",
-    id: "email",
-    name: "email",
-    type: "email",
-    label: "Email",
-    required: true,
-    placeholder: "example@gmail.com",
-  },
-  {
-    tag: "input",
-    id: "phone-number",
-    name: "phoneNumber",
-    type: "text",
-    label: "Phone Number",
-    required: true,
-    placeholder: "000-0000-0000",
-    pattern: "^[0-9]{2,3}(-|\\s)*[0-9]{4}(-|\\s)*[0-9]{4}$",
-  },
-];
-
-const initialInputs = {
-  name: "",
-  companyName: "",
-  email: "",
-  phoneNumber: "",
-  message: "",
-};
-
-const encode = (data) => {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-};
+import React from "react";
+import ContactForm from "./form/ContactForm";
 
 const Contact = () => {
-  const [formInputs, setFormInputs] = useState(initialInputs);
-
-  const onChange = (e) => {
-    setFormInputs({ ...formInputs, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contactForm", ...formInputs }),
-    })
-      .then(() => {
-        setFormInputs(initialInputs);
-        alert("Success!");
-      })
-      .catch((error) => alert(error));
-    e.preventDefault();
-  };
-
   return (
     <section className="relative mx-auto grid min-h-[calc(100vh-5rem)] max-w-[1344px] auto-rows-min grid-cols-20 gap-2 md:gap-4 lg:gap-y-0 z-0">
       <h1 className="col-start-1 col-end-8 flex h-12 items-center text-2xl md:col-start-2 md:col-end-18 lg:h-24 lg:text-3xl xl:text-4xl">
@@ -110,29 +27,7 @@ const Contact = () => {
           </div>
         </div>
         {/* Right Side (form) */}
-        <div className="col-start-2 col-end-18 md:col-end-14 lg:col-start-10 lg:col-end-18">
-          <form
-            className="flex h-full flex-col justify-center gap-4"
-            onSubmit={handleSubmit}
-          >
-            {inputList.map((input) => {
-              return (
-                <FormInput
-                  key={input.id}
-                  {...input}
-                  value={formInputs[input.name]}
-                  onChange={onChange}
-                />
-              );
-            })}
-            <button
-              type="submit"
-              className="mt-4 bg-light-theme-2 py-2 text-xl duration-200 ease-in hover:bg-light-theme-3 dark:bg-[#1b356a] dark:hover:bg-dark-theme-4"
-            >
-              Send Message
-            </button>
-          </form>
-        </div>
+        <ContactForm />
       </div>
     </section>
   );
---
+++ src/components/pages/contact/form/ContactForm.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useRef } from "react";
+import FormInput from "./FormInput";
+import { inputList } from "./inputList";
+import ReCAPTCHA from "react-google-recaptcha";
+
+const RECAPTCHA_KEY = process.env.REACT_APP_SITE_RECAPTCHA_KEY;
+
+const initialInputs = {
+  name: "",
+  companyName: "",
+  email: "",
+  phoneNumber: "",
+  message: "",
+};
+
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
+const ContactForm = () => {
+  const [formInputs, setFormInputs] = useState(initialInputs);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const recapthcaRef = useRef();
+
+  const handleChange = (e) => {
+    setFormInputs({ ...formInputs, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const recaptchaValue = recapthcaRef.current.getValue();
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": "contactForm",
+        "g-recaptcha-response": recaptchaValue,
+        ...formInputs,
+      }),
+    })
+      .then(() => {
+        setFormInputs(initialInputs);
+        alert("Success!");
+      })
+      .catch((error) => alert(error));
+  };
+
+  return (
+    <div className="col-start-2 col-end-18 md:col-end-14 lg:col-start-10 lg:col-end-18">
+      <form
+        className="flex h-full flex-col justify-center gap-4"
+        data-netlify-recaptcha="true"
+        onSubmit={handleSubmit}
+      >
+        {inputList.map((input) => {
+          return (
+            <FormInput
+              key={input.id}
+              {...input}
+              value={formInputs[input.name]}
+              onChange={handleChange}
+            />
+          );
+        })}
+        <ReCAPTCHA
+          ref={recapthcaRef}
+          sitekey={RECAPTCHA_KEY}
+          size="normal"
+          id="recaptcha-google"
+          onChange={() => {
+            setBtnDisabled(false);
+          }}
+        />
+        <button
+          type="submit"
+          className="mt-4 bg-light-theme-3 py-2 text-xl duration-200 ease-in hover:bg-[#6e95f1] disabled:bg-light-theme-2 disabled:hover:bg-light-theme-2 dark:bg-[#1b356a] dark:hover:bg-dark-theme-4 dark:disabled:bg-slate-600 dark:disabled:hover:bg-slate-600"
+          disabled={btnDisabled}
+        >
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
