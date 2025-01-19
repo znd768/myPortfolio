7ce331f59a0b8005bb720228fc2b0f05d49f1def
znd768 <rasksk37@gmail.com>
2025-01-20 00:28:43 +0900

    fix typo

--- src/components/pages/contact/form/ContactForm.jsx
+++ src/components/pages/contact/form/ContactForm.jsx
@@ -22,7 +22,7 @@ const encode = (data) => {
 const ContactForm = () => {
   const [formInputs, setFormInputs] = useState(initialInputs);
   const [btnDisabled, setBtnDisabled] = useState(true);
-  const recapthcaRef = useRef();
+  const recaptchaRef = useRef();
 
   const handleChange = (e) => {
     setFormInputs({ ...formInputs, [e.target.name]: e.target.value });
@@ -30,7 +30,7 @@ const ContactForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const recaptchaValue = recapthcaRef.current.getValue();
+    const recaptchaValue = recaptchaRef.current.getValue();
 
     fetch("/", {
       method: "POST",
@@ -67,7 +67,7 @@ const ContactForm = () => {
           );
         })}
         <ReCAPTCHA
-          ref={recapthcaRef}
+          ref={recaptchaRef}
           sitekey={RECAPTCHA_KEY}
           size="normal"
           id="recaptcha-google"
